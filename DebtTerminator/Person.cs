d0af9ae14cb0e2f233124cdfcca325408750f090
OM BIKRAM RANA <omxx0014@edu.easj.dk>
2017-09-25 13:59:20 +0200

    unit test started

--- DebtTerminator/Person.cs
+++ DebtTerminator/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DebtTerminator
 {
-    class Program
+  public   class Person
     {
         static void Main(string[] args)
         {
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DebtTerminator;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private Person _person;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _person = new Person();
+        }
+
+        [TestMethod]
+        public void AddPerson()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void AddDebt()
+        {
+            Assert.Fail();
+        }
+
+        
+    }
+}
